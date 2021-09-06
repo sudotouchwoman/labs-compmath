e289373ed38b810a7626f748412d966db99b22f6
Nikita Teterin <nekitonum@gmail.com>
2021-09-07 02:55:41 +0300

    add doc, update src

--- sem2/src/main.py
+++ sem2/src/main.py
@@ -1,5 +1,5 @@
 from utils import approx
 
 if __name__ == '__main__':
-    approx.approximate(xlim=4, stepsize=0.001)
+    approx.approximate(xlim=2, stepsize=0.001)
     approx.save_results('results')
--- sem2/src/utils/approx.py
+++ sem2/src/utils/approx.py
@@ -5,67 +5,58 @@ import math
 canvas = plt.figure(figsize=(10, 10), dpi=300)
 ax = canvas.add_subplot(111)
 
-def plot_erf(x: np.ndarray, dtype = np.float32, *fx) -> None:
+def plot_erf(x: np.ndarray, *fx) -> None:
     STYLE = {
-        'color':['#168039','#ff5252', '#ffbe43', '#A24CC2'.lower()],
+        'color':['#168039','#ff5252', '#ffbe43', '#a24cc2'],
         'linestyle':['-','--', '--', '--'],
         'legend':[
-            'erf(x)', 'lin approx', 'squared approx', 'approx from wiki'
-        ]
+            'erf(x)', '$I_{1}(x)$', '$I_{2}(x)$', '$I_M(x)$'
+        ],
+        'xlabel':'$\it{X}$',
+        'ylabel':'$\it{f(X)}$'
     }
+
     for i, f in enumerate(fx):
         ax.plot(x, f, color=STYLE['color'][i], linestyle=STYLE['linestyle'][i])
 
-    plt.xlabel('$\it{X}$')
-    plt.ylabel('$\it{erf(X)}$')
-    plt.legend(STYLE['legend'], loc='best')
-    plt.show()
-
-def plot_error(x: np.ndarray, dtype = np.float32, *fx) -> None:
-    STYLE = {
-        'legend':[
-            '|erf(x) - lin approx|', '|erf(x) - squared approx|', '|erf(x) - approx from wiki|'
-        ]
-    }
-    reference = fx[0]
-    for i, f in enumerate(fx[1:]):
-        ax.plot(x, abs(f - reference), linestyle=STYLE['linestyle'][i])
-
-    plt.xlabel('$\it{X}$')
-    plt.ylabel('Error |$\it{erf(X) - erf*(X)}$|')
+    plt.xlabel(STYLE['xlabel'])
+    plt.ylabel(STYLE['ylabel'])
     plt.legend(STYLE['legend'], loc='best')
     plt.show()
 
 def save_results(filename: str = "compared") -> None:
     canvas.savefig(filename+'.svg', format='svg')
 
-def approximate_linear(x, dtype = np.float32):
+def approximate_linear(x):
     erf_li = \
         x * ( math.exp(-x**2) + 1 ) / math.sqrt(math.pi)
     return erf_li
 
-def approximate_square(x, dtype = np.float32):
+def approximate_square(x):
     erf_sqi = \
-        x * ( math.exp(-x**2) + 1 + math.exp(-(x/2)**2) ) / 3*math.sqrt(math.pi)
-        #( 2.0 / math.sqrt(math.pi) ) * ( x + ( math.exp((-x**2) / 4) - 1 )*(2*math.exp(-x**2) - 2*math.exp((-x**2) / 4) + x) / 3 )
+        x * ( math.exp(-x**2) + 1 + 4*math.exp(-(x/2)**2) ) / 3*math.sqrt(math.pi)
     return erf_sqi
 
-def approximate_wiki(x, dtype = np.float32):
-    a = \
-        (8 / 3 * math.pi)*((3 - math.pi)/(math.pi - 4))
-    erf_sqi = \
-        math.sqrt( 1 - math.exp( (-x**2)*(4/math.pi + a*x**2)/(1 + a*x**2) ) )
-        #( 2.0 / math.sqrt(math.pi) ) * ( x + ( math.exp((-x**2) / 4) - 1 )*(2*math.exp(-x**2) - 2*math.exp((-x**2) / 4) + x) / 3 )
-        #x * ( math.exp(-x**2) + math.exp((-x**2) / 4) + 1 ) / 3*math.sqrt(math.pi)
-    return erf_sqi
+def approximate_series(x):
+    erf_series = \
+        ( -(x**7)/42 + (x**5)/10 - (x**3)/3 + x) * (2 / math.sqrt(math.pi))
+    return erf_series
 
 def approximate(xlim, stepsize = 0.01, dtype = np.float32):
     x = np.arange(xlim, step=stepsize, dtype=dtype)
     
-    erf_true =  np.array([math.erf(xi) for xi in x])
-    erf_li =    np.array([approximate_linear(xi, dtype) for xi in x])
-    erf_sqi =   np.array([approximate_square(xi, dtype) for xi in x])
-    erf_wiki =  np.array([approximate_wiki(xi, dtype) for xi in x])
+    erf_true =  np.array([math.erf(xi) for xi in x], dtype=dtype)
+    erf_li =    np.array([approximate_linear(xi) for xi in x], dtype=dtype)
+    erf_sqi =   np.array([approximate_square(xi) for xi in x], dtype=dtype)
+    erf_series =  np.array([approximate_series(xi) for xi in x], dtype=dtype)
 
-    plot_erf(x, dtype, erf_true, erf_li, erf_sqi, erf_wiki)
-    #plot_error(x, dtype, erf_true, erf_li, erf_sqi, erf_wiki)
+    plot_erf(x, erf_true, erf_li, erf_sqi, erf_series)
+
+    # for factor in [2, 4, 5, 10, 20]:
+    #     print(f'Рассчёт в диапазоне [0;{xlim}] Для x < {xlim/(factor)}')
+    #     for couple in [(erf_true, erf_li), (erf_true, erf_sqi), (erf_true, erf_series)]:
+    #         d = diff(*couple, len(x) // factor )
+    #         print(f'Максимальное отклонение {d:e}')
+
+def diff(f1, f2, xlim):
+    return max([abs(f1i - f2i) for i, (f1i, f2i) in enumerate(zip(f1, f2)) if i < xlim])
