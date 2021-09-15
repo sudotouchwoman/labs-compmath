aa0b6fd34238b779dc1847d68690d847b3591602
Nikita Teterin <nekitonum@gmail.com>
2021-09-15 12:12:15 +0300

    update README, fix paths

--- lab1/src/main-advanced.py
+++ lab1/src/main-advanced.py
@@ -1,9 +1,9 @@
 from utils import uncertainty
 
 if __name__ == '__main__':
-    h_settings = uncertainty.load_config('res/Y-config.json')
+    h_settings = uncertainty.load_config('res/cfg/Y-config.json')
     uncertainty.H_analysis(h_settings)
 
-    x_settings = uncertainty.load_config('res/X-config.json')
+    x_settings = uncertainty.load_config('res/cfg/X-config.json')
     uncertainty.X_analysis(x_settings)
     
--- lab1/src/main-base.py
+++ lab1/src/main-base.py
@@ -1,9 +1,9 @@
 from utils import interpolate
 
 if __name__ == '__main__':
-    NODES = interpolate.load_nodes('res/nodes.json')
+    NODES = interpolate.load_nodes('res/cfg/nodes.json')
     SplineModel = interpolate.CubSqplineMethod(*NODES)
-    SplineModel.interpolate('res/spline')
+    SplineModel.interpolate('res/plots/spline')
 
     LagrangeModel = interpolate.LagrangeMethod(*NODES)
-    LagrangeModel.interpolate('res/lagrange')
+    LagrangeModel.interpolate('res/plots/lagrange')
